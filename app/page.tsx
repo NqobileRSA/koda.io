8c3e097fe51d32f59682682c22adfd730c35474b
NqobileNgobeni <115787995+NqobileNgobeni@users.noreply.github.com>
2024-08-23 14:46:35 +0200

    About section in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/ui/Navbar/Navbar';
 import HomePage from '@/app/pages/HomePage';
 import FeaturesSection from './pages/FeaturesSection';
+import AboutPage from './pages/AboutPage';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <HomePage />
       <FeaturesSection />
+      <AboutPage />
     </>
   );
 }
